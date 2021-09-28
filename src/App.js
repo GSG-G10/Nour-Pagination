36a5174c5415ee7001e8ec9121cf99d0032c6e67
NourAdawi <nouradwi12@gmail.com>
2021-09-28 14:53:09 +0300

    add pagination component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React , {useState, useEffect} from 'react';
+import { Pagination } from 'antd';
 
 import './App.css';
 import 'antd/dist/antd.css'
 
 function App() {
+
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect( () => {
+    let isCancelled = false;
+
+    if(!isCancelled){
+    fetch(`https://api.unsplash.com/search/collections?page=2&limit=10&query=cat&client_id=kQ_rA8Dd9Tb-JZ80Nx6RyFBtaoIFyaP5kdLn5EmGkVM`)
+    .then(res => res.json())
+    // .then(res => console.log(res.results))
+    // .then(res => setData(res.results))
+ }
+
+ return () => {
+    isCancelled = true;
+}       
+},)
+
+
   return (
     <div className="App">
 
-      <h1> Hi</h1>
+      <h1> Explore Cats Photos</h1>
+      <Pagination defaultCurrent={1} total={50} />
   
     </div>
   );
