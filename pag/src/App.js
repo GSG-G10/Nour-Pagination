553a07b60b7993d32cf39ab05c510843041ca03a
NourAdawi <nouradwi12@gmail.com>
2021-09-28 14:41:45 +0300

    setup

---
+++ pag/src/App.js
@@ -0,0 +1,16 @@
+import React , {useState, useEffect} from 'react';
+
+import './App.css';
+import 'antd/dist/antd.css'
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1> Hi</h1>
+  
+    </div>
+  );
+}
+
+export default App;
